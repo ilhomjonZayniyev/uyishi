b9e837c4c2ec1dce5c3acc1543fb1ad66bbbbde0
ilhomjonZayniyev <ilhomjonzayniyev7@gmail.com>
2025-04-24 01:07:52 +0500

    sdhsdl

---
+++ js/add.js
@@ -0,0 +1,109 @@
+
+// m1
+// let k = 5;
+// let n = 4;
+
+// if(n>0){
+//     for(let i=1; i<=n;i++){
+//         console.log(k);
+//     }
+// }
+
+// m2
+// let a=5
+// let b=12
+// let summa=0
+// if (a<b){
+   
+//         for(let i=a; i<=b;i++){
+//             summa++
+//             console.log(i)
+            
+//         }
+//         console.log(summa)
+   
+// }
+//m3
+// let a=5
+// let b=12
+// let summa=0
+// if (a<b){
+   
+//         for(let i=b; i>=a;i--){
+//             summa++
+//             console.log(i)
+            
+//         }
+//         console.log(summa)
+   
+// }
+//m4 
+// {
+//   let konfet_1kg=prompt("narxini kigizing")   
+//   konfet_1kg=Number(konfet_1kg)
+//   for(let i=1; i<=10;i++){
+//     console.log(i+"kg="+konfet_1kg*i)
+//   }
+// }
+//m5
+// {
+//      let konfet_1kg=prompt("narxini kigizing")   
+//   konfet_1kg=Number(konfet_1kg)
+//   for(let i=0.1; i<=1;i+=0.1){
+//     console.log(i+"kg="+konfet_1kg*i)
+//   } 
+// }
+//m6
+// {
+//         let konfet_1kg=prompt("narxini kigizing")   
+//   konfet_1kg=Number(konfet_1kg)
+//   for(let i=1; i<=2;i+=0.1){
+//     console.log(i+"kg="+konfet_1kg*i)
+//    } 
+// }
+//m7
+// {
+//       let a=2
+//       let b=23
+//       let summa=0
+//       if(a<b){
+//         for(let i=a;i<=b;i++){
+//            console.log( summa+=i)
+            
+//         }
+//       }
+//   } 
+//m8
+
+// {
+//       let a=2
+//       let b=5
+//       let summa=1
+//       if(a<b){
+//         for(let i=a;i<=b;i++){
+//            console.log( summa*=i)
+            
+//         }
+//       }
+//   } 
+//m9
+
+//     {
+//       let a=2
+//       let b=10
+//       let summa=0
+//       if(a<b){
+//         for(let i=a;i<=b;i++){
+//            console.log( (i**2))
+            
+//         }
+//       }
+//   } 
+
+//m10
+let S=0
+let n=prompt("n")
+n=Number(n)
+for(let i=1;i<=n;i++){
+    console.log(S+=1/i)
+}
